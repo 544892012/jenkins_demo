8f643c0c5301ffd23016708e32f4230f6141155a
wenchaozeng <wenchaozeng@tencent.com>
2018-11-12 20:20:03 +0800

    demo

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/************************************************************************* 
+	> File Name: test.cpp
+	> Author: wenchaozeng
+	> Desc: 
+	> Created Time: Mon 12 Nov 2018 08:15:09 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    cout<< "Hello World"<< endl;
+
+    return 0;
+}
